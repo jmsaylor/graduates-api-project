e1bd55f5c29dc450ba9d97d989d41ca5908f2e8c
John <johnmsaylor@gmail.com>
2020-02-24 20:01:02 -0500

    comments

--- public/script.js
+++ public/script.js
@@ -8,7 +8,12 @@ function showReviews(e) {
     xhrGet.onload = () => {
       const parsedData = JSON.parse(xhrGet.responseText);
       console.log(parsedData);
+
+      //This for loop will create an entry for each review
+      //And its delete button
       for (let x = 0; x < parsedData.length; x++) {
+        //Create an element called item and fill it with info
+        //about each review
         let item = document.createElement("li");
         item.id = parsedData[x]._id;
         item.innerHTML = parsedData[x].name;
@@ -16,18 +21,24 @@ function showReviews(e) {
         item.innerHTML += parsedData[x].gradDate;
         item.innerHTML += "<br>";
         item.innerHTML += parsedData[x].review;
+
+        //Then we make a button and attach it to the item
         let button = document.createElement("button");
         button.innerHTML = "delete";
-
+        item.appendChild(button);
+        //A click will trigger this function
         item.addEventListener("click", e => {
           e.preventDefault();
+          //this is where we take the id of each item so we know which one to delete
           let id = parsedData[x]._id;
           console.log(id);
           try {
             let xhrDelete = new window.XMLHttpRequest();
             xhrDelete.open("DELETE", `http://localhost:3000/api/grads/${id}`);
             xhrDelete.onload = () => {
+              //this clears everything on the page
               document.body.innerHTML = "";
+              //and then calls this again to reflect the deletion
               showReviews(e);
             };
             xhrDelete.send();
@@ -35,7 +46,7 @@ function showReviews(e) {
             console.error(error);
           }
         });
-        item.appendChild(button);
+
         document.body.appendChild(item);
       }
     };
